35a66883ccfef7d578b59db6c3374947aa50d92d
michel-chieregato-einstein <163673439+michel-chieregato-einstein@users.noreply.github.com>
2024-03-17 22:36:48 -0300

    Update exercicio_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -82,3 +82,20 @@ Digite a frequencia alélica (em porcentagem): 100
 
 Resposta: Não é relevante.
 """
+frequencia_populacional = float(input('Digite a frequencia populacional (em porcentagem):'))
+gene = input('Digite o gene:')
+impacto = input('Digite a Impacto (ALTO ou BAIXO):')
+reads = float(input('Digite os reads:'))
+frequencia_alelica = float(input('Digite a frequencia alélica (em porcentagem):'))
+
+eh_artefato = (frequencia_alelica < 20) or (reads < 10)
+impacto_baixo = impacto == 'BAIXO'
+esta_em_genes_de_excecao = (gene == 'HFE') or (gene == 'MEFV') or (gene == 'GJB2')
+frequencia_populacional_alta = frequencia_populacional > 5
+
+if eh_artefato:
+    print('Não é relevante.')
+elif impacto_baixo or (frequencia_populacional_alta and not esta_em_genes_de_excecao):
+    print('Não é relevante.')
+else:
+    print('É relevante.')
